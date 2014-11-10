b305a6b7125d982e0fffeb41ec32027d63f67170
David Maas <contact@pathogenstudios.com>
2014-11-09 19:04:22 -0600

    Added skeleton functions for testing covalent bonds. Added testing commutative-ness of reactions.

--- periodic-tests/TestStep_Bonds.cpp
+++ periodic-tests/TestStep_Bonds.cpp
@@ -3,7 +3,20 @@
 
 #include "Element.h"
 
-// Cation is the positively charged ion.
+void __TestCovalentBond(const char* a, const char* b, int numElectronsShared, const char* message)
+{
+    Element ea;
+    Element eb;
+    TestMessage(message);
+    TestEqBool("Get the first element", Element::GetRawElement(a, &ea), true);
+    TestEqBool("Get the second element", Element::GetRawElement(b, &eb), true);
+
+    ea.ReactWith(&eb);
+    //TODO: IMEPLEMENT ME
+    TestForceFail("It is currently impossible to check for a covalent bond in this build.");
+}
+
+// Note: Cation is the positively charged ion.
 void __TestIonicBond(const char* cation, const char* anion, int numElectronsDonated, const char* message)
 {
     Element c;
@@ -18,12 +31,39 @@ void __TestIonicBond(const char* cation, const char* anion, int numElectronsDona
     TestEqInt("Check the anion's charge after the reaction", a.GetCharge(), -numElectronsDonated);
 }
 
-#define TestIonicBond(cation, anion, numElectronsDonated) __TestIonicBond(cation, anion, numElectronsDonated, "React " cation " and " anion " with expected charge of " #numElectronsDonated)
+#define _TestCovalentBond(a, b, numElectronsShared) __TestCovalentBond(a, b, numElectronsShared, "React " a " and " b " with expected electrons shared count of " #numElectronsShared)
+#define _TestIonicBond(cation, anion, numElectronsDonated) __TestIonicBond(cation, anion, numElectronsDonated, "React " cation " and " anion " with expected charge of " #numElectronsDonated)
+
+// Automatically test that all reactions are commutative
+#define TestCovalentBond(a, b, numElectronsShared) _TestCovalentBond(a, b, numElectronsShared); _TestCovalentBond(b, a, numElectronsShared)
+#define TestIonicBond(cation, anion, numElectronsDonated) _TestIonicBond(cation, anion, numElectronsDonated); _TestIonicBond(anion, cation, -numElectronsDonated)
 
 void TestStep_Bonds()
 {
     TestMessage("Test covalent bonds");
     TestForceFail("It is currently impossible to check for a covalent bond in this build.");
+    #if 0
+    TestCovalentBond("H", "H", 1);
+    TestCovalentBond("Li", "H", 1);
+    TestCovalentBond("Na", "H", 1);
+    TestCovalentBond("K", "H", 1);
+    TestCovalentBond("Cs", "H", 1);
+    TestCovalentBond("Fr", "H", 1);
+    TestCovalentBond("F", "F", 1);
+    TestCovalentBond("Cl", "Cl", 1);
+    TestCovalentBond("Br", "Br", 1);
+    TestCovalentBond("I", "I", 1);
+    TestCovalentBond("F", "Cl", 1);
+    TestCovalentBond("F", "Br", 1);
+    TestCovalentBond("F", "I", 1);
+    TestCovalentBond("Cl", "Br", 1);
+    TestCovalentBond("Cl", "I", 1);
+    TestCovalentBond("H", "F", 1);
+    TestCovalentBond("H", "Cl", 1);
+    TestCovalentBond("H", "Br", 1);
+    TestCovalentBond("H", "I", 1);
+    TestCovalentBond("H", "At", 1);
+    #endif
 
     TestMessage("Test ionic compounds");
     TestIonicBond("Li", "F", 1);
