3f77e00beccf78034943dee0e79589678eb7b376
Alex L'Esperance <alesperance22@gmail.com>
2015-03-10 12:30:57 -0500

    Cleaned up Node.cpp.  Fixed the merging errors I forgot to last week

--- periodic/Node.cpp
+++ periodic/Node.cpp
@@ -7,14 +7,11 @@ Node::Node()
 	mMarker = false;
 }
 
-<<<<<<< HEAD
 Node::Node(int i)
 {
     mContent = ' ';
     mMarker = false;
 }
-=======
->>>>>>> e8569f07b15b8cea73157a3a617b89e07f6e80d4
 
 
 Node::~Node()
@@ -27,11 +24,7 @@ void Node::setContent(char c) { mContent = c; }
 
 bool Node::getReactionMarker() { return mMarker; }
 
-<<<<<<< HEAD
-void Node::setReactionMarker(bool marker) { mMarker = marker; }
-=======
 void Node::setReactionMarker() { mMarker = true; }
->>>>>>> e8569f07b15b8cea73157a3a617b89e07f6e80d4
 
 void Node::appendChild(Node* child) { mChildren.push_back(child); }
 
